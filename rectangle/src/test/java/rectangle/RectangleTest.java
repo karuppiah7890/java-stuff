f32d9f4c41bb7d14d77e92d6f7b07f5dc3b2e9b7
Karuppiah Natarajan <karuppiah7890@gmail.com>
2020-07-04 10:02:49 +0530

    add one more test for area method

--- rectangle/src/test/java/rectangle/RectangleTest.java
+++ rectangle/src/test/java/rectangle/RectangleTest.java
@@ -19,4 +19,9 @@ class RectangleTest {
         Rectangle rectangle = new Rectangle(1, 1);
         assertEquals(1, rectangle.area());
     }
+
+    @Test void shouldGiveTwoWhenLengthIsOneAndBreadthIsTwo() {
+        Rectangle rectangle = new Rectangle(1, 2);
+        assertEquals(2, rectangle.area());
+    }
 }
